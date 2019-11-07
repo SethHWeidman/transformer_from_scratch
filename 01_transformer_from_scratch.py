2517ba666f4e20f33fc1accaee252c4f9612e520
Seth Weidman <sethweidman@fb.com>
2019-11-07 14:13:23 -0800

    Transformer with input and output

---
+++ 01_transformer_from_scratch.py
@@ -0,0 +1,35 @@
+'''
+Defines a Transformer using torch.nn and feeds input data through it.
+Illustrates the correct input shapes for a Transformer.
+'''
+
+import torch
+import torch.nn as nn
+
+
+if __name__=='__main__':
+    # source sequence length
+    S = 10
+
+    # target sequence length
+    T = 20
+
+    # batch size
+    N = 32
+
+    # feature size
+    # note: assumes tokens have already been passed through an embedding layer
+    E = 50
+
+    # number of heads
+    # necessary since feature size has to be divisible by the number of heads 
+    H = 5
+
+    src = torch.rand(S, N, E)
+    tgt = torch.rand(T, N, E)
+
+    t = nn.Transformer(d_model = E,
+                       nhead = H)
+
+    out = t(src, tgt)
+    print(out.shape)
